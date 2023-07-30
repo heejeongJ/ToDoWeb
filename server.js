f93713a5dd2e56e0d2f0280809c5dc711d94bad8
hee_!J <heeej920@naver.com>
2023-07-30 22:43:44 +0900

    Add main, list page
    Add main, list page

---
+++ server.js
@@ -0,0 +1,19 @@
+// 서버 띄우기 위한 기본 세팅 (express 라이브러리)
+
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){        // listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
+    console.log('listening on 8080')
+});
+
+
+
+app.get('/TodoList', function(req, res){
+    //res.send('반갑습니다.');
+    res.sendFile(__dirname + '/ToDoList.html')
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+});
